3a577765ee768e125e17d9f3e2d451f688e2b5c3
stfn12 <stefandobos1995@yahoo.com>
2019-01-19 11:02:07 +0200

    Revert "api models"
    This reverts commit 50b8d90e724b3e5a2f9279d39077860547e980f2.

--- api/src/models/mongo/ProductModel.js
+++ api/src/models/mongo/ProductModel.js
@@ -1,22 +1,4 @@
-const mongoose = require('mongoose');
-
-let productSchema = mongoose.Schema(
-    {
-        _id: {type: Number, required: true},
-        data: JSON
-    },
-    {
-        strict: false
-    }
-);
-
-productSchema.methods.getProduct = function getProduct() {
-    return {
-        id: this._id,
-        data: this.data
-    }
-}
-
-let Product = mongoose.model('Product', productSchema);
-
+const mongoose = require('mongoose')
+let productSchema = mongoose.Schema({_id: {type: Number, required: true}, data: JSON}, {strict: false})
+let Product = mongoose.model('Product', productSchema)
 export default Product
