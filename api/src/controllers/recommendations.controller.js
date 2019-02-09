5967b5fcd4f56e6416810099768c089b520efca6
stfn12 <stefandobos1995@yahoo.com>
2019-02-09 21:50:55 +0200

    improved recommendations

--- api/src/controllers/recommendations.controller.js
+++ api/src/controllers/recommendations.controller.js
@@ -2,13 +2,22 @@ import Product from '../models/mongo/ProductModel'
 import Favourite from '../models/mongo/FavouriteModel'
 import { getImage } from './products.controller'
 
+export const KEYWORDS_TRANSLATIONS = {
+  'beverage': ['boisson'],
+  'food': ['aliments'],
+  'sweet': ['bonbon'],
+  'snack': ['colation'],
+  'dairie': ['laiterie'],
+  'meat': ['viande']
+}
+
 export const queryProducts = async (req, pId) => {
   let query = {}
   let {
     categories, country, packages, nutrition, fat,
     salt, sugar, sfat
   } = req.query
-  if(pId){
+  if (pId) {
     query._id = {$in: pId}
   }
   if (country !== 'all') {
@@ -37,12 +46,31 @@ export const queryProducts = async (req, pId) => {
     terms = [...terms, ...categories]
   }
   if (req.favs.length > 0) {
-    let keywords = await Product.distinct('_keywords', {_id: {$in: req.favs}}).exec()
+    let innerQuery = {_id: {$in: req.favs}}
+    if (terms.length > 0) {
+      let innerTerms = []
+      for (let i = 0; i < terms.length; i++) {
+        innerTerms.push(terms[i])
+        if (KEYWORDS_TRANSLATIONS[innerTerms[i]]) {
+          innerTerms = [...innerTerms, ...KEYWORDS_TRANSLATIONS[innerTerms[i]]]
+        }
+      }
+      innerTerms = innerTerms.map(term => `^${term}`)
+      innerQuery._keywords = {$regex: innerTerms.join('|')}
+    }
+    let keywords = await Product.distinct('_keywords', innerQuery).exec()
     terms = [...terms, ...keywords]
   }
   if (terms.length > 0) {
-    terms = terms.map(term => `^${term}`)
-    query._keywords = {$regex: terms.join('|')}
+    let innerTerms = []
+    for (let i = 0; i < terms.length; i++) {
+      innerTerms.push(terms[i])
+      if (KEYWORDS_TRANSLATIONS[innerTerms[i]]) {
+        innerTerms = [...innerTerms, ...KEYWORDS_TRANSLATIONS[innerTerms[i]]]
+      }
+    }
+    innerTerms = innerTerms.map(term => `^${term}`)
+    query._keywords = {$regex: innerTerms.join('|')}
   }
   console.log(JSON.stringify(query))
   let products = await Product.find(query).limit(5).exec()
@@ -58,15 +86,14 @@ export const queryProducts = async (req, pId) => {
   return products
 }
 
-
 export const getRecommendations = async (req, res) => {
   try {
     let products = []
-    if(req.query.friends_ids){
-      let favs = await Favourite.distinct('products', {user_id: {$in: req.query.friends_ids }}).exec()
+    if (req.query.friends_ids) {
+      let favs = await Favourite.distinct('products', {user_id: {$in: req.query.friends_ids}}).exec()
       products = await queryProducts(req, favs)
     }
-    if(products.length<10){
+    if (products.length < 10) {
       products = [...products, ...(await  queryProducts(req))]
     }
     return res.json(products)
